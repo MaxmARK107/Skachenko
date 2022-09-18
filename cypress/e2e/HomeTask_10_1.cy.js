ea1ff3ab246a979abd172355cf060ff6cdbdd192
MaxmARK107 <maximskachenko@gmail.com>
2022-09-18 21:35:44 +0300

    Max10

--- cypress/e2e/HomeTask_10_1.cy.js
+++ cypress/e2e/HomeTask_10_1.cy.js
@@ -1,6 +1,7 @@
 describe('Calculator check on main page', () => {
     before(() => {
         cy.visit('https://www.beaxy.com');
+        cy.viewport(1300,768);
         cy.get('div[class="widget-wrapp__main"]').scrollIntoView({offset:{top:-100}});
         
     });
@@ -10,17 +11,20 @@ describe('Calculator check on main page', () => {
         cy.get('div[class="widget__input-data"]',{timeout:3000}).find('input').invoke('val').should('eq', '100');        
         cy.get('div[class="widget__input-data-currency"]').invoke('text').should('eq', 'USD');
         cy.get('.select-block-active-quote__value').invoke('text').should('eq', 'BTC');
-        cy.get('.cacl-btn-name-crypto').invoke('text').should('eq', 'Bitcoin');
-        //cy.get('span[class="widget__output-data"]').invoke('text').should('eq', '0.00497196');       
+        cy.get('.cacl-btn-name-crypto').invoke('text').should('eq', 'Bitcoin');        
+        //cy.get('span[class="widget__output-data"]').invoke('text').should('eq', '0.00497196');    
+        //Не знаю, где взять текущий курс крипты, чтобы проверить, правильно ли работает калькулятор   
     });
-    it('Calculator work check', () => {
-        cy.get('div[class="widget-wrapp__main"]').scrollIntoView({offset:{top:-100}});
+    it('Calculator work check', () => {         
+        console.log('BTC currency = ', typeof(currentBTC))
+        cy.get('div[class="widget-wrapp__main"]').scrollIntoView({offset:{top:-100}});                
         cy.get('div[class="widget__input-data"]').find('input').clear().type('250');
         cy.get('input[class="widget__input-data-value"]').invoke('val').should('eq', '250');
-        //cy.get('span[class="widget__output-data"]').invoke('text').should('eq', '250*0.00497196'); 
+        cy.get('span[class="widget__output-data"]').invoke('text').should('eq', calcResult); 
         cy.get('.widget__select-block .widget__select-block-btn').click();
         cy.url().should('eq', 'https://www.beaxy.com/register/');
         cy.visit('https://www.beaxy.com');
         cy.get('div[class="widget-wrapp__main"]').scrollIntoView({offset:{top:-100}});
+        
     });
 });
