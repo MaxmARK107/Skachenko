6e6061a3ac69e023fb523968eee0452d79df34f8
MaxmARK107 <maximskachenko@gmail.com>
2022-10-13 16:36:55 +0300

    Max17

--- cypress/e2e/Lesson_17.cy.js
+++ cypress/e2e/Lesson_17.cy.js
@@ -1,8 +1,29 @@
 describe ('Visual Tests', () => {
-    it ('Test_01', () => {
+    it ('HomePage', () => {
         cy.viewport(1950,1600);
         cy.visit('https://www.monobank.ua');
         cy.wait(5000);
+        cy.matchImageSnapshot();
+    });
+    it ('MobileApp_02', () => {
+        cy.viewport(1950,1600);
+        cy.visit('https://www.monobank.ua');
+        cy.get('[class="tab-link"]').eq(0).click();
+        cy.get('[class="line-in-view"]', {timeout:5000}).eq(3).should('be.visible');
+        cy.get('[class="line-in-view"]').eq(3).matchImageSnapshot();
+    });
+    it ('MobileApp_03', () => {
+        cy.viewport(1950,1600);
+        cy.visit('https://www.monobank.ua');
+        cy.get('[class="tab-link"]').eq(1).click();
+        cy.get('[class="line-in-view"]', {timeout:5000}).eq(3).should('be.visible');
+        cy.get('[class="line-in-view"]').eq(3).matchImageSnapshot();
+    });
+    it.only ('Beaxy HomePage', () => {
+        cy.visit('https://www.beaxy.com');
+        cy.wait(10000);
+        cy.get('[class="home-table"]').invoke('remove');
+        cy.get('[class="widget__output-data-wrapp"]').invoke('remove');
         cy.matchImageSnapshot();
     });
 });
