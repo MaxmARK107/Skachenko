fbe29217151eb591e51e1dec92062ad13dbd546e
MaxmARK107 <maximskachenko@gmail.com>
2022-10-27 19:36:03 +0300

    Max21

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,16 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
     addMatchImageSnapshotPlugin(on, config);
     },
+    excludeSpecPattern: ['**/cypress/e2e/1-getting-started', '**/cypress/e2e/2-advanced-examples'],
+    baseUrl: 'https://www.beaxy.com',
+    defaultCommandTimeout: 2000,
+    retries: 1,
+    redirectionLimit: 2,
+    watchForFileChanges: true,
+    pageLoadTimeout: 8000,
+    responseTimeout: 2000,
+    screenshotOnRunFailure: false,
+    slowTestThreshold: 10000,
+    
   },
 });
